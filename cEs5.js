9980b9da4615203425015f8c1c61e267fcc6f0c1
Jacob Papageorgiou <jacob@ten4design.co.uk>
2019-06-06 11:28:24 +0100

    JS course init

---
+++ cEs5.js
@@ -0,0 +1,107 @@
+var john = {
+	fullName: 'John Wick',
+	bills: [124, 48, 268, 180, 42],
+	calcTips: function() {
+		this.tips = [];
+		this.finalValues = [];
+
+		for(var i = 0; i < this.bills.length; i++) {
+			var percentage;
+			var bill = this.bills[i]; // inside the counter, the index value of each bill is determined by the [i]. So, this.bills[i] can be 0, 1, 2, 3, 4 of the bills array at any given time while the for loop is running. Let's store that in a var called bill.
+
+			if (bill < 50) {
+				percentage = 0.2;
+			} else if (bill >= 50 && bill < 200) {
+				percentage = 0.10;
+			} else {
+				percentage = 0.25;
+			}
+
+			this.tips[i] = bill * percentage; // get the tip at any given point in the loop during the counter, then store it in tips[i]
+			this.finalValues[i] = bill + bill * percentage;
+
+		}
+	}
+}
+
+var mark = {
+	fullName: 'Mark Miller',
+	bills: [77, 375, 110, 45],
+	calcTips: function() {
+		this.tips = [];
+		this.finalValues = [];
+
+		for(var i = 0; i < this.bills.length; i++) {
+			var percentage;
+			var bill = this.bills[i]; // inside the counter, the index value of each bill is determined by the [i]. So, this.bills[i] can be 0, 1, 2, 3, 4 of the bills array at any given time while the for loop is running. Let's store that in a var called bill.
+
+			if (bill < 100) {
+				percentage = 0.2;
+			} else if (bill >= 100 && bill < 300) {
+				percentage = 0.10;
+			} else {
+				percentage = 0.25;
+			}
+
+			this.tips[i] = bill * percentage; // get the tip at any given point in the loop during the counter, then store it in tips[i]
+			this.finalValues[i] = bill + bill * percentage;
+
+		}
+	}
+}
+
+function calcAverage(tipzorz) {
+    var sum = 0;
+    for(var i = 0; i < tipzorz.length; i++) {
+        sum += tipzorz[i];
+    }
+    return sum / tipzorz.length;
+}
+
+john.calcTips();
+mark.calcTips();
+console.log(john, mark);
+
+/*
+CRUD: "Update" - just like any object mutation, we can assign calcAverage(john.tips),
+which will run through the calcTips key:value to fetch the value of this.tips array and assign it to "average".
+*/
+
+john.average = calcAverage(john.tips);
+mark.average = calcAverage(mark.tips);
+console.log(john, mark);
+
+if (john.average > mark.average) {
+	console.log(john.fullName + "'s family paid a higher tip on average with $" + john.average);
+} else if (mark.average > john.average) {
+	console.log(mark.fullName + "'s family paid a higher tip on average with $" + mark.average);
+}
+
+// Mark Miller's family paid a higher tip on average with $32.2875
+
+
+/*
+
+EXPLANATION
+
+*** calcTip() function of John & Mark objects ***
+
+* `function()` has no parameters as not needed.
+* Keyword: `this` - `this.tips` & `this.finalValues` - refers to those specific values of those specific arrays, and not anything else (within the scope of the john/mark object, NOT the global .window object).
+* `this.bills.length` - determines this.bills' values first (L to R), then the length, in our case length == 5;
+* `this.bills[i]` - used to determine the value of the specific counter variable in that specific bills array
+* Since we use `this.bills[i]` for every condition and we want to stay DRY, we store it in a `var bill = this.bills[i]` and reuse it
+* `this.tips[i]` (line 41) simply means the tip at the current position of the array, using the counter variable equals `bill * percentage` to give us the amount of tip owed
+* `this.finalValues[i]` at that current position will equal the bill + the tip. JS precedence operators work like basic math ones, so `bill * percentage` will be calculated first and then the original bill, from the array will be added to it.
+* Finally, we call calcTips() on the john variable and log it to the console to get the results.
+
+*** calcAverage() function explained ***
+
+Declare a function with a parameter tips, which will be an array of tips. We create a functio here and not a method to prevent repeating ourselves and writing out code again.
+The function can be reused when called with an argument (john.tips and mark.tip in this case).
+Assign sum = 0 outside the loop so it holds a value and stays/doesn't restart everytime the loop runs.
+Start the loop and run it until i < tips.length and at the tip[i] of that current position, add it to sum. So the sum = 0 will take it's new value.
+Then the loop will run again, and add the new tip value at that current position to the sum.
+To calc. the average we return the total sum / length of the tips.
+
+*/
---
+++ cEs6.js
@@ -0,0 +1,44 @@
+/*
+
+Tip calculator
+
+Joe and his family went to 5 different restaurants. The bills were $124, $48, $268, $180 and $42.
+Joe likes to tip 20% when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+Implement a tip calculator using objects and loops:
+
+1. Create an object with an array for the bill values
+2. Add a method to calculate the tip
+3. This method should include a loop to iterate over all the paid bills and do the tip calculations
+4. As an output, create 1) a new array containing all tips, and 2) an array containing final paid amounts (bill + tip). HINT: Start with two empty arrays [] as properties and then fill them up in the loop.
+
+EXTRA: Mat's family also went on a holiday, going to 4 different restaurants. The bills were $77, $375, $110, and $45.
+
+Mat likes to tip 20% of the bill when the bill is less than $100, 10% when the bill is between $100 and $300, and 25% if the bill is more than $300 (different than Joe).
+5. Implement the same functionality as before, this time using Mat's tipping rules
+6. Create a function (not a method) to calculate the average of a given array of tips. HINT: Loop over the array, and in each iteration store the current sum in a variable (starting from 0). After you have the sum of the array, divide it by the number of elements in it (that's how you calculate the average)
+7. Calculate the average tip for each family
+8. Log to the console which family paid the highest tips on average
+
+*/
+
+const joe = {
+    fullName: "Joe Reed",
+    bills: [124, 48, 268, 180, 42],
+    calcTip: () => {
+        this.tips = [];
+        this.finalValues = [];
+        for(let i = 0; i < this.bills.length; i++){
+
+            let percentage;
+            let bill = this.bills[i]; // to resemble the value of a bill in the array when looping, we use the i counter variable
+
+            if (bill < 50) {
+                percentage = 0.2;
+            } else if (bill >= 50 && bill < 200) {
+                percentage = 0.15;
+            }
+        }
+
+    }
+}
