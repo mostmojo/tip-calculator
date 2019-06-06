f80174f6d10c66fa6e363b65481ddda5c5e9f314
Jacob Papageorgiou <jacob@ten4design.co.uk>
2019-06-06 12:20:27 +0100

    Add second object with tip function

--- cEs6.js
+++ cEs6.js
@@ -1,17 +1,5 @@
 /*
 
-Tip calculator
-
-Joe and his family went to 5 different restaurants. The bills were $124, $48, $268, $180 and $42.
-Joe likes to tip 20% when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
-
-Implement a tip calculator using objects and loops:
-
-1. Create an object with an array for the bill values
-2. Add a method to calculate the tip
-3. This method should include a loop to iterate over all the paid bills and do the tip calculations
-4. As an output, create 1) a new array containing all tips, and 2) an array containing final paid amounts (bill + tip). HINT: Start with two empty arrays [] as properties and then fill them up in the loop.
-
 EXTRA: Mat's family also went on a holiday, going to 4 different restaurants. The bills were $77, $375, $110, and $45.
 
 Mat likes to tip 20% of the bill when the bill is less than $100, 10% when the bill is between $100 and $300, and 25% if the bill is more than $300 (different than Joe).
@@ -49,3 +37,31 @@ const joe = {
 
 joe.calcTip();
 console.log(joe);
+
+const mat = {
+    fullName: "Mat Jeez",
+    bills: [77, 375, 110, 45],
+    calcTip: function() {
+        this.tips = [];
+        this.finalValues = [];
+        for(let i = 0; i < this.bills.length; i++){
+
+            let percentage;
+            let bill = this.bills[i];
+
+            if (bill < 100) {
+                percentage = 0.2;
+            } else if (bill >= 100 && bill < 300) {
+                percentage = 0.1;
+            } else {
+                percentage = 0.25;
+            }
+            this.tips[i] = bill * percentage;
+            this.finalValues = bill + (bill * percentage);
+        }
+
+    }
+}
+
+mat.calcTip();
+console.log(mat);
